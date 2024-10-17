195aecc0abcc5864568bdaf8457619b4df5fd870
manu <manuel.nto.rmo@gmail.com>
2024-10-17 21:43:14 +0200

    Unable to use sbrk so decided to use mmap, simple malloc working

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 int main()
 {
     int *num = (int *)my_malloc(sizeof(int));
+    int *num2 = (int *)my_malloc(sizeof(int));
+    char *ch = (char *)my_malloc(sizeof(int));
+    float *fl = (float *)my_malloc(sizeof(int));
     if (num == NULL) 
     {
         printf("Error al asignar memoria.\n");
--- myMalloc.c
+++ myMalloc.c
@@ -1,47 +1,51 @@
-#include <unistd.h>
+#define _DEFAULT_SOURCE
+#include <sys/mman.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 #include "myMalloc.h"
+
 
 Block *head = NULL;
 
 void *my_malloc(size_t size) 
 {
-
-    if(size == 0)
+    if (size == 0) 
     {
-        printf("\nerror\n");
+        printf("\nError: tamaño 0 no es válido\n");
         return NULL;
     }
-    
+
     size_t totalSize = (size + sizeof(Block) + 7) & ~7;
-    void *ptr = sbrk(totalSize);
-    
-    if(ptr == (void *)-1)
+
+    void *ptr = mmap(NULL, totalSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS , -1, 0);
+
+    if (ptr == MAP_FAILED) 
     {
+        printf("\nError: no se pudo asignar memoria con mmap\n");
         return NULL;
     }
-    
-    Block *newBlock = (Block *)ptr;
 
+    Block *newBlock = (Block *)ptr;
     newBlock->free = 0;
     newBlock->size = size;
     newBlock->next = NULL;
 
-    printf("\nBloque asignado en: %p, tamaño solicitado: %zu\n", newBlock, size);
-    
+    printf("\nBloque asignado en: %p, tamaño solicitado: %zu\n", (void *)newBlock, size);
 
-
-    if(head == NULL)
+    if (head == NULL) 
     {
         head = newBlock;
-    } else 
+    } 
+    else 
     {
         Block *aux = head;
-        while(aux->next != NULL){
+        while (aux->next != NULL) 
+        {
             aux = aux->next;
-        };
+        }
         aux->next = newBlock;
     }
 
